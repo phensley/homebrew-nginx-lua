a79759d28f87b311ad1e3fee55c34f4f9b748df4
Patrick Hensley <phensley@squarespace.com>
2016-02-01 10:43:50 -0500

    Upgrades to openresty 1.9.7.3 and modules.

--- Formula/nginx-lua.rb
+++ Formula/nginx-lua.rb
@@ -5,16 +5,18 @@ class NginxLua < Formula
   homepage 'http://openresty.org/'
 
   S3BASE = "http://s3.amazonaws.com/v6.nginx/sources"
+  GLONK = 'http://glonk.com'
 
   stable do
-    url 'http://openresty.org/download/ngx_openresty-1.7.7.1.tar.gz'
-    sha1 'bf70d465710f4d7a7aac24daa1841265bdc7e4e1'
+    url 'https://openresty.org/download/openresty-1.9.7.3.tar.gz'
+    sha1 '1a2029e1c854b6ac788b4d734dd6b5c53a3987ff'
   end
 
-  resource "upstream_check" do
-    url "#{S3BASE}/nginx_upstream_check_module.tar.gz"
-    sha1 '36d5b11d744b9fd399cc79042445b63218087a35'
-  end
+# replaced by pure lua healthcheck module
+#  resource "upstream_check" do
+#    url "http://glonk.com/nginx_upstream_check_module-0.3.0.tar.gz"
+#    sha1 '2fabab3e5c253e950a02202c883f0f9dfec01848'
+#  end
 
   resource "jvm_route" do
     url "#{S3BASE}/nginx-upstream-jvm-route.tar.gz"
@@ -37,41 +39,37 @@ class NginxLua < Formula
   end
 
   resource "lua_healthcheck" do
-    url "#{S3BASE}/lua-resty-upstream-healthcheck.tar.gz"
-    sha1 'cb34250a30e2f16cbc5ecb2580fdcbaa80321bad'
+    url "#{GLONK}/lua-resty-upstream-healthcheck.tar.gz"
+    sha1 '609c925e3c4611114a76222ca916722d7319736d'
   end
 
   resource "upstream_cache" do
-    url  "http://glonk.com/lua-upstream-cache-nginx-module.tar.gz"
+    url  "#{GLONK}/lua-upstream-cache-nginx-module.tar.gz"
     sha1 '9d53f81bb3ccaf7270ddeab65462febae3635b8e'
   end
 
   depends_on 'openssl'
   depends_on 'pcre'
-  depends_on 'geoip' => :optional
+  depends_on 'geoip'
 
   # options
-  option 'with-iconv', "Compile with support for converting character encodings"
   option 'with-debug', "Compile with support for debug logging but without proper gdb debugging symbols"
-
-  # nginx options
-  option 'with-webdav', "Compile with ngx_http_dav_module"
-  option 'with-gunzip', "Compile with ngx_http_gunzip_module"
-  option 'with-geoip', "Compile with ngx_http_geoip_module"
-  option 'with-stub_status', "Compile with ngx_http_stub_status_module"
 
   skip_clean 'logs'
 
   def install
 
-    args = ["--prefix=#{prefix}",
-      "--with-http_ssl_module",
-      "--with-luajit",
-      "--with-pcre",
-      "--with-pcre-jit",
-      "--with-http_gunzip_module",
-      "--with-http_realip_module",
-      "--with-http_stub_status_module",
+    args = [
+      "--prefix=#{prefix}",
+      '--with-http_ssl_module',
+      '--with-luajit',
+      '--with-pcre',
+      '--with-pcre-jit',
+      '--with-http_gunzip_module',
+      '--with-http_realip_module',
+      '--with-http_geoip_module',
+      '--with-http_gzip_static_module',
+      '--with-http_stub_status_module',
       "--sbin-path=#{bin}/nginx-lua",
       "--conf-path=#{etc}/nginx-lua/nginx.conf",
       "--pid-path=#{var}/run/nginx-lua.pid",
@@ -81,18 +79,20 @@ class NginxLua < Formula
     args << "--with-http_dav_module" if build.with? 'webdav'
     args << "--with-http_geoip_module" if build.with? 'geoip'
 
-    %w[upstream_check jvm_route lua_idn lua_marshal lua_mongo lua_healthcheck upstream_cache].each do |r|
+    %w[jvm_route lua_idn lua_marshal lua_mongo lua_healthcheck upstream_cache].each do |r|
       resource(r).stage do
         (buildpath+r).install Dir["*"]
       end
     end
 
-    cd buildpath/"bundle/nginx-1.7.7" do
-      system "patch -p1 < #{buildpath}/upstream_check/check_1.7.5+.patch"
+    cd buildpath/"bundle/nginx-1.9.7" do
+# replaced by pure lua healthcheck
+#      system "patch -p1 < #{buildpath}/upstream_check/check_1.9.2+.patch"
       system "patch -p0 < #{buildpath}/jvm_route/jvm_route.patch"
     end
 
-    args << "--add-module=#{buildpath}/upstream_check"
+# replaced by pure lua healthcheck
+#    args << "--add-module=#{buildpath}/upstream_check"
     args << "--add-module=#{buildpath}/jvm_route"
     args << "--add-module=#{buildpath}/upstream_cache"
 
@@ -103,18 +103,10 @@ class NginxLua < Formula
       args << '--with-no-pool-patch'
       
       # this allows setting of `debug.sethook` in luajit
-      unless build.without? 'luajit'
-        args << '--with-luajit-xcflags=-DLUAJIT_ENABLE_CHECKHOOK'
-      end
+      args << '--with-luajit-xcflags=-DLUAJIT_ENABLE_CHECKHOOK'
       
       opoo "Openresty will be built --with-debug option, but without debugging symbols. For debugging symbols you have to compile it by hand."
     end
-
-    # OpenResty options
-    args << "--with-lua51" if build.without? 'luajit'
-
-    args << "--with-http_postgres_module" if build.with? 'postgresql'
-    args << "--with-http_iconv_module" if build.with? 'iconv'
 
     system "./configure", *args
 
@@ -122,7 +114,7 @@ class NginxLua < Formula
     system "make install"
  
     # build marshal native lua lib
-    luajitpath = buildpath/"bundle/LuaJIT-2.1-20141128"
+    luajitpath = buildpath/"bundle/LuaJIT-2.1-20160108"
     cd buildpath/"lua_marshal" do
       system "gcc -O3 -I#{luajitpath}/src -o lmarshal.o -c lmarshal.c"
       system "gcc -bundle -undefined dynamic_lookup -o marshal.so lmarshal.o"
